e51b41e714e16c030eb34d60f37608bb71638d2c
Ghadeer91 <>
2021-05-05 11:47:43 +0000

    adding js

---
+++ app.js
@@ -0,0 +1,18 @@
+var query = prompt ('Are you vegan?')
+if (query === 'Yes'){alert('Good for you!')
+}else if (query ==='No'){alert('Welcome to vegans world!')
+}else{alert('thank you for being here')}
+
+
+
+
+
+var query = prompt('Wanna check a vegan recipe?')
+
+if(query === 'yes'){
+  document.write('<img src="4.jpeg">')
+}else if(query === 'mercedes'){
+  document.write('You must know that it is totally worth it!')
+}else {
+  alert('Thank you!')}
+  
